d05727c6c7b0a96249d915e6369d488e470e9fa0
hamed <hamedgorji1368@gmail.com>
2022-05-21 12:06:13 +0430

    add

---
+++ precache-manifest.a4920e6452b827043e3f430d4116f282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411bb9ce87c49f3f6372",
+    "url": "/css/chunk-vendors.9e40fd9d.css"
+  },
+  {
+    "revision": "406e0263b4ead3bab949bd9edea36521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "701c93821bad8f5c85a7",
+    "url": "/js/app.6593e647.js"
+  },
+  {
+    "revision": "411bb9ce87c49f3f6372",
+    "url": "/js/chunk-vendors.aab55e85.js"
+  },
+  {
+    "revision": "303ed9df1044745384e4a314d61743bd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
